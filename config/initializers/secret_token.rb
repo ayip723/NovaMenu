dc142b03a343a09121b699aab5374172ae35b41f
Arthur Yip <arthuryip723@gmail.com>
2014-03-26 15:20:00 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NovaMenu::Application.config.secret_key_base = '8cce0867351e3419cf202e80968d9de07f7fd6e962c4ca6245dd57fc3f7c9025579b42faca89bc2411901fb85783f4ceb9cc46794b40d168bd0388a6bd1f0fc7'
